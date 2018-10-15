77bb1517a6461ff873ec7c73a9b5b7e90473e98a
Thiago Amaral <thiago-amaral@msn.com>
2018-10-15 14:18:38 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/thiago/cursomc/domain/Cliente.java
+++ src/main/java/com/thiago/cursomc/domain/Cliente.java
@@ -35,6 +35,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -87,6 +90,12 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+ 	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -110,5 +119,4 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-
 }
