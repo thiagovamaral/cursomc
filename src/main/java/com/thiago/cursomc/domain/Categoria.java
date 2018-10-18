7138fdfdbea4711f2264899739ae9d18b4594652
Thiago Amaral <thiago-amaral@msn.com>
2018-10-18 13:57:39 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/thiago/cursomc/domain/Categoria.java
+++ src/main/java/com/thiago/cursomc/domain/Categoria.java
@@ -31,7 +31,7 @@ public class Categoria implements Serializable {
 	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	public String getNome() {
