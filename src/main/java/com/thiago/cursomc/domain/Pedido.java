65ebeb0084ae69c87f74909a31bf5bee18458be6
Thiago Amaral <thiago-amaral@msn.com>
2018-10-31 09:54:35 -0300

    Implementando toString de Pedido

--- src/main/java/com/thiago/cursomc/domain/Pedido.java
+++ src/main/java/com/thiago/cursomc/domain/Pedido.java
@@ -1,9 +1,13 @@
 package com.thiago.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +17,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -49,6 +54,7 @@ public class Pedido implements Serializable {
 	
 	public Pedido() {
 	}
+	
 	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
 		this.id = id;
@@ -56,42 +62,55 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public Date getInstante() {
 		return instante;
 	}
+	
 	public void setInstante(Date instante) {
 		this.instante = instante;
 	}
+	
 	public Pagamento getPagamento() {
 		return pagamento;
 	}
+	
 	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
 	}
+	
 	public Cliente getCliente() {
 		return cliente;
 	}
+	
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+	
 	public Endereco getEnderecoDeEntrega() {
 		return enderecoDeEntrega;
 	}
+	
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
 	public Set<ItemPedido> getItens() {
 		return itens;
 	}
+	
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -99,6 +118,7 @@ public class Pedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -114,5 +134,29 @@ public class Pedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+	
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		
+		for (ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
 	}
 }
