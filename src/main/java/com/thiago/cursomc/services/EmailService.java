2725dd36497a529b985845694498c3a815bc0c6b
Thiago Amaral <thiago-amaral@msn.com>
2018-10-31 13:15:33 -0300

    Email HTML

--- src/main/java/com/thiago/cursomc/services/EmailService.java
+++ src/main/java/com/thiago/cursomc/services/EmailService.java
@@ -1,11 +1,16 @@
 package com.thiago.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
+
 import com.thiago.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);	
 	void sendEmail(SimpleMailMessage msg);
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
--- src/main/java/com/thiago/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/thiago/cursomc/services/SmtpEmailService.java
@@ -1,16 +1,22 @@
 package com.thiago.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
 	private MailSender mailSender;
-
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
 	@Override
@@ -19,4 +25,12 @@ public class SmtpEmailService extends AbstractEmailService {
 		mailSender.send(msg);
 		LOG.info("Email enviado");
 	}
+	
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
+	}
+	
 }
