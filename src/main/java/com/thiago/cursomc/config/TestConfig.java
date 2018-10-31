8ebafdeebbd4dbae9ac6c7751abb690b441159a0
Thiago Amaral <thiago-amaral@msn.com>
2018-10-31 10:17:51 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/thiago/cursomc/config/TestConfig.java
+++ src/main/java/com/thiago/cursomc/config/TestConfig.java
@@ -1,15 +1,20 @@
 package com.thiago.cursomc.config;
 
 import java.text.ParseException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
 import com.thiago.cursomc.services.DBService;
+import com.thiago.cursomc.services.EmailService;
+import com.thiago.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
+	
 	@Autowired
 	private DBService dbService;
 	
@@ -17,5 +22,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
