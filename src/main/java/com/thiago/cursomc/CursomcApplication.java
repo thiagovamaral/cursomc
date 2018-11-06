8ebbdc83bd5ab190e9c42f89b0779881d469cc84
Thiago Amaral <thiago-amaral@msn.com>
2018-11-06 13:28:28 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/thiago/cursomc/CursomcApplication.java
+++ src/main/java/com/thiago/cursomc/CursomcApplication.java
@@ -1,18 +1,24 @@
 package com.thiago.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.thiago.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\Thiago Amaral\\Desktop\\ana.jpg");
 	}
-} 
+}
