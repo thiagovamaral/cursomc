78e96ca6e2aa24258873d0717ff50d3f36919d16
Thiago Amaral <thiago-amaral@msn.com>
2018-11-06 14:49:24 -0200

    Usando padrão de nomes para imagens

--- src/main/java/com/thiago/cursomc/domain/Cliente.java
+++ src/main/java/com/thiago/cursomc/domain/Cliente.java
@@ -37,8 +37,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	private String imageUrl;
-	
 	@JsonIgnore
 	private String senha;
 	
@@ -150,14 +148,6 @@ public class Cliente implements Serializable {
 	
 	public void addPerfil(Perfil perfil) {
 		perfis.add(perfil.getCod());
-	}
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-	
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
  	
 	@Override
