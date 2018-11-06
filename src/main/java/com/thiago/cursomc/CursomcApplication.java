8531d3293a649359d25585db4e2e192a410e13ef
Thiago Amaral <thiago-amaral@msn.com>
2018-11-06 14:12:12 -0200

    Enviando imagem via endpoint

--- src/main/java/com/thiago/cursomc/CursomcApplication.java
+++ src/main/java/com/thiago/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.thiago.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.thiago.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,6 +13,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\Thiago Amaral\\Desktop\\ana.jpg");
+
 	}
 }
