f5aaefcc087605bb1061d47bf3811bcbd3506953
Thiago Amaral <thiago-amaral@msn.com>
2018-11-07 12:51:45 -0200

    Endpoints para buscar estados e cidades

--- src/main/java/com/thiago/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/thiago/cursomc/repositories/EstadoRepository.java
@@ -1,9 +1,17 @@
 package com.thiago.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.thiago.cursomc.domain.Estado;
 
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+	@Transactional(readOnly=true)
+	public List<Estado> findAllByOrderByNome();
+	
 }
