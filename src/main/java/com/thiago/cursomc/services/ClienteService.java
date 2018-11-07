76bb2af1c7669dafabcc423a6f2d1dda823c642b
Thiago Amaral <thiago-amaral@msn.com>
2018-11-07 12:26:06 -0200

    Endpoint para buscar cliente por email

--- src/main/java/com/thiago/cursomc/services/ClienteService.java
+++ src/main/java/com/thiago/cursomc/services/ClienteService.java
@@ -66,6 +66,20 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	public Cliente findByEmail(String email) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+	
+		Cliente obj = repo.findByEmail(email);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
 	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
